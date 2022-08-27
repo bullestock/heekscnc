8efcfa07c078cd53be92a1c028240a90ec66efb9
Torsten Martinsen <torsten@bullestock.net>
2022-08-27 23:15:41 +0200

    Set Python module path.

--- src/PythonStuff.h
+++ src/PythonStuff.h
@@ -14,28 +14,28 @@ class Property;
 class CPyProcess : public wxProcess
 {
 protected:
-  int m_pid;
+	int m_pid;
 
 public:
-  CPyProcess(void);
-  static bool redirect;
+	CPyProcess();
+	static bool redirect;
 
-  void Execute(const wxChar* cmd);
-  void Cancel(void);
-  void OnTerminate(int pid, int status);
-  void OnTimer(wxTimerEvent& WXUNUSED(event));
+	void Execute(const wxChar* cmd);
+	void Cancel();
+	void OnTerminate(int pid, int status);
+	void OnTimer(wxTimerEvent& WXUNUSED(event));
 
-  virtual void ThenDo(void) { }
+	virtual void ThenDo() { }
 
 private:
-  wxTimer m_timer;
-  void HandleInput(void);
-
+	wxTimer m_timer;
+	wxExecuteEnv m_env;
+	void HandleInput();
 };
 
 bool HeeksPyPostProcess(const CProgram* program, const wxString &filepath, const bool include_backplot_processing);
 bool HeeksPyBackplot(const CProgram* program, HeeksObj* into, const wxString &filepath);
-void HeeksPyCancel(void);
+void HeeksPyCancel();
 
 
 class CSendToMachine : public CPyProcess
@@ -46,14 +46,13 @@ class CSendToMachine : public CPyProcess
 public:
 	static wxString m_command;
 
-	CSendToMachine(void) { };
+	CSendToMachine() { }
 	void SendGCode(const wxChar *gcode);
 	void Cancel();
 
-	static void GetOptions(std::list<Property *> *list);
+	static void GetOptions(std::list<Property*>* list);
 	static void ReadFromConfig();
 	static void WriteToConfig();
 };
 
-bool HeeksSendToMachine(const wxString &gcode);
-
+bool HeeksSendToMachine(const wxString& gcode);
